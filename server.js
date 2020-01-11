a676a6a2e85328fa0eacc65b9c7fe0e562bc164e
Eric Hamil <erichamil@Erics-MacBook-Pro.local>
2020-01-11 10:22:19 -0500

    semi completed code

---
+++ server.js
@@ -0,0 +1,45 @@
+var express = require("express");
+var mongoose = require("mongoose");
+var exphbs = require("express-handlebars");
+
+// Require all models
+//var db = require("./models");
+
+var PORT = process.env.PORT || 3100;
+
+// Initialize Express
+var app = express();
+
+// Parse request body as JSON
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+// Make public a static folder
+app.use(express.static("public"));
+
+// Handlebars
+app.engine(
+  "handlebars",
+  exphbs({
+    defaultLayout: "main",
+    extname:'.handlebars'
+  })
+);
+app.set('views','./views');
+app.set("view engine", "handlebars");
+
+// Connect to the Mongo DB
+// If deployed, use the deployed database. Otherwise use the local mongoHeadlines database
+var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/articles";
+
+mongoose.connect(MONGODB_URI,{ useNewUrlParser: true });
+
+//Routes
+require("./routes/apiRoutes")(app);
+
+// Start the server
+app.listen(PORT, function() {
+  console.log("App running on port " + PORT + "!");
+ });
+  
+
+module.exports = app;
